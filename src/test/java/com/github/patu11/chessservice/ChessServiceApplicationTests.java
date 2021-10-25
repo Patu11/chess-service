df3d476d15c48f243bf7f4502d6624caa7c588fe
Patryk Warchoł <patryk.t.warchol@gmail.com>
2021-10-25 13:22:22 +0200

    initial commit

---
+++ src/test/java/com/github/patu11/chessservice/ChessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.patu11.chessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
